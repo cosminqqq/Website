c509e11c08f0ed404e39d459e6743cedcae96bde
cosminqqq <48871216+cosminqqq@users.noreply.github.com>
2020-01-15 00:37:36 +0200

    Update jsBig.js

--- jsBig.js
+++ jsBig.js
@@ -1,7 +1,12 @@
  	$('.icon').click( function () {   
 			$('nav').toggleClass('open-nav', '0');
-	
+		
+		var x = document.querySelector('.open-nav ul');
+		x.style.height = window.innerHeight+"px";
+
 	});
+	
+	
 	
 
 				
